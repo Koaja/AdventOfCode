b3af8aa9b818d5e931fde51f1a74f1872f07c408
Koaja <Koaja@KoajaHome>
2015-12-04 15:15:33 +0200

    .

--- AdventOfCode/src/day2/Day2.java
+++ AdventOfCode/src/day2/Day2.java
@@ -13,7 +13,6 @@ public class Day2 {
 		int w = 0;
 		int h = 0;
 
-		int ribbonNeeded = 0;
 		int papperNeeded = 0;
 		int extraPapper = 0;
 
@@ -24,6 +23,7 @@ public class Day2 {
 
 		while ((line = bf2.readLine()) != null) {
 			String[] dim = line.split("x");
+			
 			l = Integer.parseInt(dim[0]);
 			w = Integer.parseInt(dim[1]);
 			h = Integer.parseInt(dim[2]);
@@ -32,33 +32,37 @@ public class Day2 {
 			int boxSideB = 2 * l * w;
 			int boxSideC = 2 * h * w;
 
-			int smallerSide = getSmallerSide(boxSideA, boxSideB, boxSideC);
+			System.out.println("Box: " + line); //box dimensions
+			
+			int smallerSide = getSmallerSide(boxSideA, boxSideB, boxSideC); // stores the smallest side / extra papper for each present 
 			System.out.println("Smallest side: " + smallerSide);
-			
-			int boxArea = boxSideA + boxSideB + boxSideC;
+
+			int boxArea = boxSideA + boxSideB + boxSideC; // box area for each present
 			System.out.println("Box Area: " + boxArea);
-			
-//			int ribbonPerBox = 2 * smallerSide;
-//			System.out.println("Ribbon Per box: " + ribbonPerBox);
-//			ribbonNeeded = ribbonNeeded + ribbonPerBox;
-//			System.out.println("Total RibbonNeeded: " + ribbonNeeded);
-			
-			extraPapper = extraPapper + smallerSide;
+
+			extraPapper = extraPapper + smallerSide; // total extra papper 
 			System.out.println("Total extra papper: " + extraPapper);
-			
-			papperNeeded = papperNeeded + boxArea;
-			System.out.println("Total fix papper: " + papperNeeded);
-			
+
+			papperNeeded = papperNeeded + boxArea; // total papper required
+			System.out.println("Total exact papper: " + papperNeeded);
+
 			System.out.println("----");
 		}
-		System.out.println("Total papper:" + (papperNeeded + extraPapper + ribbonNeeded));
+		System.out.println("Total papper:" + (papperNeeded + extraPapper));
 
 	}
 
+	/**
+	 * 
+	 * @param sideA
+	 * @param sideB
+	 * @param sideC
+	 * @return the smaller side
+	 */
 	public static int getSmallerSide(int sideA, int sideB, int sideC) {
-		if (sideA < sideB && sideA < sideC) {
+		if (sideA <= sideB && sideA <= sideC) {
 			return sideA;
-		} else if (sideB < sideA && sideB < sideC) {
+		} else if (sideB <= sideA && sideB <= sideC) {
 			return sideB;
 		}
 		return sideC;
